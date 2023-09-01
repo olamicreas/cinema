d211c197797ebff0d47a0597238a809ceb5472dc
olamicreas <70097198+olamicreas@users.noreply.github.com>
2023-09-01 21:21:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, abort, jsonify, send_file
+from flask import Flask, render_template, request, flash, redirect, url_for, session, abort, jsonify, send_file, send_from_directory
 import os
 import requests
 import time
@@ -36,7 +36,7 @@ mail = Mail(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return send_from_directory('static', 'v.html')
 
 @app.route('/about')
 def about():
